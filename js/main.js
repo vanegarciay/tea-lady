4f32f3b832fa6f0163138e49c3fc57f732b887c3
VaneGarciaYara <vanegarciay@gmail.com>
2017-05-15 00:02:12 -0400

    validaciones para que todos los campos no queden vacios

--- js/main.js
+++ js/main.js
@@ -1,3 +1,28 @@
+function submit(){
+    if(validarData()) {
+        mostrarData();
+    }
+}
+
+function validarData(){
+    var nombre = document.getElementById("nombre").value;
+    var apellido = document.getElementById("apellido").value;
+    var correo = document.getElementById("correo").value;
+    var direccion = document.getElementById("direccion").value;
+    var telefono = document.getElementById("telefono").value;
+    var pack = document.getElementById("tipos_te").value;
+    var suscribir = document.getElementById("suscribirse").checked;
+
+    if (nombre=="" || apellido=="" || correo=="" || direccion=="" || telefono=="" || pack=="" || suscribir==""){
+        document.getElementById("validaciones").innerHTML = 'Todos los campos deben estar llenos';
+        return false;
+    } else {
+        document.getElementById("validaciones").innerHTML = '';
+        return true;
+    }
+
+}
+
 function mostrarData(){
     var nombre = document.getElementById("nombre").value;
     var apellido = document.getElementById("apellido").value;
@@ -8,11 +33,11 @@ function mostrarData(){
     var suscribir = document.getElementById("suscribirse").checked;
 
     document.getElementById('clientes').innerHTML = '<h3>Los datos ingresados fueron: </h3>' + 
-        '<p><strong>Nombre: </strong>' + nombre + '</p>' + 
-        '<p><strong>Apellido: </strong>'+ apellido + '</p>' + 
-        '<p><strong>Correo: </strong>' + correo + '</p>' + 
-        '<p><strong>Dirección: </strong>' + direccion + '</p>' + 
-        '<p><strong>Teléfono: </strong>' + telefono + '</p>'+
-        '<p><strong>Pack: </strong>' + pack + '</p>' +
-        '<p><strong>Recibir notificaciones: </strong>' + suscribir + '</p>';
+    '<p><strong>Nombre: </strong>' + nombre + '</p>' + 
+    '<p><strong>Apellido: </strong>'+ apellido + '</p>' + 
+    '<p><strong>Correo: </strong>' + correo + '</p>' + 
+    '<p><strong>Dirección: </strong>' + direccion + '</p>' + 
+    '<p><strong>Teléfono: </strong>' + telefono + '</p>'+
+    '<p><strong>Pack: </strong>' + pack + '</p>' +
+    '<p><strong>Recibir notificaciones: </strong>' + suscribir + '</p>';
 }
